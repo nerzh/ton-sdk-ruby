ad6b1222e719f2d2ec18546c0d3e2e19a50365fc
nerzh <emptystamp@gmail.com>
2023-08-18 05:55:16 +0200

    require classes

--- lib/ton-sdk-ruby.rb
+++ lib/ton-sdk-ruby.rb
@@ -1,7 +1,23 @@
 require_relative './ton-sdk-ruby/version'
 require_relative './ton-sdk-ruby/BitArray/BitArray'
-require_relative './ton-sdk-ruby/Boc/Mask'
+
+require_relative './ton-sdk-ruby/Utils/helpers'
 require_relative './ton-sdk-ruby/Utils/Bits'
+require_relative './ton-sdk-ruby/Utils/Checksum'
+require_relative './ton-sdk-ruby/Utils/Hash'
+require_relative './ton-sdk-ruby/Utils/Numbers'
+require_relative './ton-sdk-ruby/Utils/Numbers'
+
+require_relative './ton-sdk-ruby/types/Address'
+require_relative './ton-sdk-ruby/types/Block'
+require_relative './ton-sdk-ruby/types/Coins'
+
+require_relative './ton-sdk-ruby/Boc/Mask'
+require_relative './ton-sdk-ruby/Boc/Cell'
+require_relative './ton-sdk-ruby/Boc/Hashmap'
+require_relative './ton-sdk-ruby/Boc/Slice'
+require_relative './ton-sdk-ruby/Boc/Builder'
+
 
 module TonSdkRuby
 end
