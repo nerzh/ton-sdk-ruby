9fe42ddd7c42935e13a4e69f5a000bab9901aa72
nerzh <emptystamp@gmail.com>
2023-08-18 02:22:23 +0200

    add Hash

---
+++ lib/ton-sdk-ruby/Utils/Hash.rb
@@ -0,0 +1,15 @@
+require 'digest'
+require_relative 'helpers.rb'
+
+module TonSdkRuby
+
+  def sha256(bytes)
+    digest = Digest::SHA256.digest(bytes)
+    bytes_to_hex(digest)
+  end
+
+  def sha512(bytes)
+    digest = Digest::SHA512.digest(bytes)
+    bytes_to_hex(digest)
+  end
+end
