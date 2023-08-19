a832a5ff26b5087c27e1630c22093977bbd19ae5
nerzh <emptystamp@gmail.com>
2023-08-19 04:23:57 +0200

    fix Bits

--- lib/ton-sdk-ruby/Utils/Bits.rb
+++ lib/ton-sdk-ruby/Utils/Bits.rb
@@ -1,18 +1,19 @@
 module TonSdkRuby
 
-  def augment(bits: [], divider: 8)
+  def augment(bits, divider = 8)
+    bits = Array.new(bits)
     amount = divider - (bits.size % divider)
     overage = Array.new(amount, 0)
     overage[0] = 1
 
-    if (overage.size != 0 && overage.size != divider)
+    if overage.size != 0 && overage.size != divider
       return bits.concat(overage)
     end
 
     bits
   end
 
-  def rollback(bits: [])
+  def rollback(bits)
     index = bits.last(7).reverse.index(1)
 
     if index.nil?
