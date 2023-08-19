1deb4b02dbd8f0f14ddc8083cf55c31e6b9cc62e
nerzh <emptystamp@gmail.com>
2023-08-19 04:24:21 +0200

    fix Hash

--- lib/ton-sdk-ruby/Utils/Hash.rb
+++ lib/ton-sdk-ruby/Utils/Hash.rb
@@ -4,12 +4,12 @@ require_relative 'helpers.rb'
 module TonSdkRuby
 
   def sha256(bytes)
-    digest = Digest::SHA256.digest(bytes)
+    digest = Digest::SHA256.digest(bytes.pack('C*'))
     bytes_to_hex(digest)
   end
 
   def sha512(bytes)
-    digest = Digest::SHA512.digest(bytes)
+    digest = Digest::SHA512.digest(bytes.pack('C*'))
     bytes_to_hex(digest)
   end
 end
