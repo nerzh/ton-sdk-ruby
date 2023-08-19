0ff6c6f161717aeabe41db7f473ce0ac3e2cd51d
nerzh <emptystamp@gmail.com>
2023-08-19 04:24:48 +0200

    temp test

--- spec/manual_test.rb
+++ spec/manual_test.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pp'
 include TonSdkRuby
 
 describe TonSdkRuby do
@@ -6,6 +7,21 @@ describe TonSdkRuby do
   end
 
   it 'manual_test' do
+
+    b = Builder.new
+    b
+      .store_uint(200, 30)
+      # .store_coins(Coins.new(1_000_000))
+
+    b2 = Builder.new.store_ref(b.cell)
+
+    # b2.cell
+    bytes =  TonSdkRuby.serialize(b2.cell)
+
+    p TonSdkRuby.bytes_to_base64(bytes)
+    p ""
+    pp TonSdkRuby.deserialize(bytes)
+
     # p augment(bits: [1, 0 ,1], divider: 8)
     # p rollback(bits: [0,0,0])
   end
