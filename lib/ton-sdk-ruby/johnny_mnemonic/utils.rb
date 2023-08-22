6e78a0eed3b5dcd15d6292ad5af8d048a42e295b
nerzh <emptystamp@gmail.com>
2023-08-22 04:22:13 +0200

    temp mnemonic method for bip39

--- lib/ton-sdk-ruby/johnny_mnemonic/utils.rb
+++ lib/ton-sdk-ruby/johnny_mnemonic/utils.rb
@@ -1,9 +1,12 @@
 require_relative './utils'
 require 'securerandom'
 require 'json'
+require 'openssl'
+require 'openssl/pkcs5'
 
 module TonSdkRuby
-  def derive_checksum_bits(entropy_bytes)
+
+  private def derive_checksum_bits_bip39(entropy_bytes)
     cs = (entropy_bytes.size * 8) / 32
     hex = sha256(entropy_bytes)
     bits = hex_to_bits(hex)
@@ -11,7 +14,7 @@ module TonSdkRuby
     bits.slice(0, cs)
   end
 
-  def generate_words(length = 24)
+  def generate_words_bip39(length)
     current_file_path = File.expand_path(File.dirname(__FILE__))
     bip0039en = JSON.parse(File.read("#{current_file_path}/words/english.json"))
     entropy = SecureRandom.random_bytes(bytes_needed_for_words(length)).unpack('C*')
@@ -27,7 +30,7 @@ module TonSdkRuby
     words
   end
 
-  def bytes_needed_for_words(word_count)
+  def bytes_needed_for_words_bip39(word_count)
     full_entropy = word_count * 11
     checksum = full_entropy % 32
     initial_entropy = full_entropy - checksum
