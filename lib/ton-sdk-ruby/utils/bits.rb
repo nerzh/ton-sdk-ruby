bfc735ef8e017f710de064d69ed101170f1c9cb1
nerzh <emptystamp@gmail.com>
2023-10-14 21:05:52 +0200

    version 0.0.7

--- lib/ton-sdk-ruby/utils/bits.rb
+++ lib/ton-sdk-ruby/utils/bits.rb
@@ -23,3 +23,15 @@ module TonSdkRuby
     bits[0, bits.length - (index + 1)]
   end
 end
+
+class FalseClass
+  def to_i
+    0
+  end
+end
+
+class TrueClass
+  def to_i
+    1
+  end
+end
--- lib/ton-sdk-ruby/version.rb
+++ lib/ton-sdk-ruby/version.rb
@@ -1,4 +1,4 @@
 module TonSdkRuby
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
 
